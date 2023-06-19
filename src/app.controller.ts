1ca17937a788775980d0bab63ab87a2f36524bff
kimjinho1 <rlawlsgh8113@naver.com>
2023-06-20 04:18:32 +0900

    feat: 초기 화면 모든 차량 API 구현

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello()
   }
+
+  @Get('/menu')
+  async getMenu(): Promise<any> {
+    return await this.appService.getMenu()
+  }
 }
