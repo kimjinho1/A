f0d5c67e3c0dbc3bffe54ec11edd43ca573659e2
kimjinho1 <rlawlsgh8113@naver.com>
2023-06-25 05:57:07 +0900

    feat: 차량 모델 선택 필터들 반환 API 구현

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Query } from '@nestjs/common'
+import { Controller, Get, Param, Query } from '@nestjs/common'
 import { AppService } from './app.service'
 
 @Controller()
@@ -7,43 +7,50 @@ export class AppController {
 
   @Get()
   getHello(): string {
-    return this.appService.getHello()
+    return '내 차 만들기!!'
   }
 
-  @Get('/menu')
-  async getMenu(): Promise<any> {
-    return await this.appService.getMenu()
+  // 차량 정보 반환
+  @Get('/menus')
+  async getMenus(): Promise<any> {
+    return await this.appService.getMenus()
   }
 
-  @Get('/intColor')
-  async getIntColorsByModelCode(@Query('modelCode') modelCode: string): Promise<any> {
-    return await this.appService.getIntColorsByModelCode(modelCode)
+  // 차량 모델 선택 필터들 반환
+  @Get('/model-filters/:carCode')
+  async getModelFilters(@Param('carCode') carCode: string): Promise<any> {
+    return await this.appService.getModelFilters(carCode)
   }
 
-  @Get('/extColor')
-  async getExtColorsByIntColorCode(@Query('intColorCode') intColorCode: string): Promise<any> {
-    return await this.appService.getExtColorsByIntColorCode(intColorCode)
-  }
+  // @Get('/intColor')
+  // async getIntColorsByModelCode(@Query('modelCode') modelCode: string): Promise<any> {
+  //   return await this.appService.getIntColorsByModelCode(modelCode)
+  // }
 
-  @Get('/option')
-  async getOptionsByModelCode(@Query('modelCode') modelCode: string): Promise<any> {
-    return await this.appService.getOptionsByModelCode(modelCode)
-  }
+  // @Get('/extColor')
+  // async getExtColorsByIntColorCode(@Query('intColorCode') intColorCode: string): Promise<any> {
+  //   return await this.appService.getExtColorsByIntColorCode(intColorCode)
+  // }
 
-  @Get('/disable-option')
-  async getDisabledOptionsBySelectedOptionCodes(@Query('optionCodes') optionCodes: string): Promise<any> {
-    const optionCodeList = optionCodes.split(',')
-    return await this.appService.getDisabledOptionsBySelectedOptionCodes(optionCodeList)
-  }
+  // @Get('/option')
+  // async getOptionsByModelCode(@Query('modelCode') modelCode: string): Promise<any> {
+  //   return await this.appService.getOptionsByModelCode(modelCode)
+  // }
 
-  @Get('/tuix')
-  async getTuixsByOptionCode(@Query('modelCode') modelCode: string): Promise<any> {
-    return await this.appService.getTuixsByOptionCode(modelCode)
-  }
+  // @Get('/disable-option')
+  // async getDisabledOptionsBySelectedOptionCodes(@Query('optionCodes') optionCodes: string): Promise<any> {
+  //   const optionCodeList = optionCodes.split(',')
+  //   return await this.appService.getDisabledOptionsBySelectedOptionCodes(optionCodeList)
+  // }
 
-  @Get('/add-possible-option')
-  async getAddPosibleTuixBySelectedOptionCode(@Query('optionCodes') optionCodes: string): Promise<any> {
-    const optionCodeList = optionCodes.split(',')
-    return await this.appService.getAddPosibleTuixBySelectedOptionCode(optionCodeList)
-  }
+  // @Get('/tuix')
+  // async getTuixsByOptionCode(@Query('modelCode') modelCode: string): Promise<any> {
+  //   return await this.appService.getTuixsByOptionCode(modelCode)
+  // }
+
+  // @Get('/add-possible-option')
+  // async getAddPosibleTuixBySelectedOptionCode(@Query('optionCodes') optionCodes: string): Promise<any> {
+  //   const optionCodeList = optionCodes.split(',')
+  //   return await this.appService.getAddPosibleTuixBySelectedOptionCode(optionCodeList)
+  // }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,26 +1,50 @@
 import { Injectable } from '@nestjs/common'
+import { Prisma } from '@prisma/client'
 import { PrismaService } from './prisma.service'
+
+type CarWithIncludes = Prisma.CarGetPayload<{
+  include: {
+    carEngine: {
+      include: {
+        engine: true
+      }
+    }
+    carMission: {
+      include: {
+        mission: true
+      }
+    }
+    carDrive: {
+      include: {
+        drive: true
+      }
+    }
+    carTrim: {
+      include: {
+        trim: true
+      }
+    }
+  }
+}>
 
 @Injectable()
 export class AppService {
   constructor(private prisma: PrismaService) {}
 
-  getHello(): string {
-    return 'Hello World!'
-  }
-
-  async getMenu(): Promise<any> {
+  async getMenus(): Promise<any> {
     const carList = await this.prisma.car.findMany()
     const data = Promise.all(
       carList.map(async car => {
-        const type = await this.prisma.carType.findUnique({
+        const carType = await this.prisma.carType.findUnique({
           where: {
             carTypeId: car.carTypeId
           }
         })
-        if (!type) {
-          return
+
+        if (!carType) {
+          throw new Error('carType not found')
         }
+
         const carModels = await this.prisma.carModel.findMany({
           where: {
             carId: car.carId
@@ -36,164 +60,207 @@ export class AppService {
         return {
           carCode: car.carCode,
           carName: car.carName,
-          carTypeName: type.carTypeName,
+          carTypeName: carType.carTypeName,
           carLowPrice: carModels[0].modelPrice
         }
       })
     )
+
     return data
   }
 
-  async getIntColorsByModelCode(modelCode: string) {
-    // const modelCode = 'NXJJ5DCT2'
-    // const extColorCode = 'B6S'
-
-    const carModel = await this.prisma.carModel.findUnique({
-      where: { modelCode },
-      select: {
-        carId: true,
-        trimId: true
-      }
-    })
-
-    if (!carModel) {
-      throw new Error('CarModel not found')
-    }
-
-    const { carId, trimId } = carModel
-
-    const intColors = await this.prisma.carTrimIntColor.findMany({
+  async getModelFilters(carCode: string): Promise<any> {
+    const car: CarWithIncludes | null = await this.prisma.car.findUnique({
       where: {
-        carId,
-        trimId
+        carCode
       },
-      select: {
-        intColor: true
-      }
-    })
-    return intColors
-  }
-
-  async getExtColorsByIntColorCode(intColorCode: string) {
-    const intColor = await this.prisma.intColor.findUnique({
-      where: {
-        intColorCode
-      }
-    })
-
-    if (!intColor) {
-      throw new Error('intColor not found')
-    }
-
-    const extColors = await this.prisma.intExtColor.findMany({
-      where: { intColorId: intColor.intColorId },
-      select: {
-        extColor: true
-      }
-    })
-
-    return extColors.map(extColor => extColor.extColor)
-  }
-
-  async getOptionsByModelCode(modelCode: string) {
-    const carModel = await this.prisma.carModel.findUnique({
-      where: {
-        modelCode
-      }
-    })
-
-    if (!carModel) {
-      throw new Error('carModel not found')
-    }
-
-    const options = await this.prisma.carModelOption.findMany({
-      where: { modelId: carModel.modelId },
-      select: {
-        option: true
-      }
-    })
-
-    return options.map(option => option.option)
-  }
-
-  async getDisabledOptionsBySelectedOptionCodes(optionCodes: string[]) {
-    const data: any = []
-    await Promise.all(
-      optionCodes.map(async optionCode => {
-        const option = await this.prisma.option.findUnique({
-          where: {
-            optionCode
+      include: {
+        carEngine: {
+          include: {
+            engine: true
           }
-        })
-
-        if (!option) {
-          throw new Error('option not found')
+        },
+        carMission: {
+          include: {
+            mission: true
+          }
+        },
+        carDrive: {
+          include: {
+            drive: true
+          }
+        },
+        carTrim: {
+          include: {
+            trim: true
+          }
         }
-
-        const disabledOptions = await this.prisma.disabledOption.findMany({
-          where: {
-            optionId: option.optionId
-          },
-          select: {
-            disabledOption: true
-          }
-        })
-        disabledOptions.map(disabledOption => {
-          data.push(disabledOption.disabledOption)
-        })
-      })
-    )
-    return data
-  }
-
-  async getTuixsByOptionCode(modelCode: string) {
-    const carModel = await this.prisma.carModel.findUnique({
-      where: {
-        modelCode
       }
     })
 
-    if (!carModel) {
-      throw new Error('carModel not found')
+    if (!car) {
+      throw new Error('car not found')
     }
 
-    const tuixs = await this.prisma.carModelTuix.findMany({
-      where: { modelId: carModel.modelId },
-      select: {
-        tuix: true
-      }
-    })
-
-    return tuixs.map(tuix => tuix.tuix)
+    const result = {
+      engine: car.carEngine.map(t => t.engine),
+      mission: car.carMission.map(t => t.mission),
+      drive: car.carDrive.map(t => t.drive),
+      trim: car.carTrim.map(t => t.trim)
+    }
+    return result
   }
 
-  async getAddPosibleTuixBySelectedOptionCode(optionCodes: string[]) {
-    const data: any = []
-    await Promise.all(
-      optionCodes.map(async optionCode => {
-        const option = await this.prisma.option.findUnique({
-          where: {
-            optionCode
-          }
-        })
+  // async getIntColorsByModelCode(modelCode: string) {
+  //   // const modelCode = 'NXJJ5DCT2'
+  //   // const extColorCode = 'B6S'
 
-        if (!option) {
-          throw new Error('option not found')
-        }
+  //   const carModel = await this.prisma.carModel.findUnique({
+  //     where: { modelCode },
+  //     select: {
+  //       carId: true,
+  //       trimId: true
+  //     }
+  //   })
 
-        const tuixRequiredOptions = await this.prisma.tuixRequiredOption.findMany({
-          where: {
-            optionId: option.optionId
-          },
-          select: {
-            tuix: true
-          }
-        })
-        tuixRequiredOptions.map(tuixRequiredOption => {
-          data.push(tuixRequiredOption.tuix)
-        })
-      })
-    )
-    return data
-  }
+  //   if (!carModel) {
+  //     throw new Error('CarModel not found')
+  //   }
+
+  //   const { carId, trimId } = carModel
+
+  //   const intColors = await this.prisma.carTrimIntColor.findMany({
+  //     where: {
+  //       carId,
+  //       trimId
+  //     },
+  //     select: {
+  //       intColor: true
+  //     }
+  //   })
+  //   return intColors
+  // }
+
+  // async getExtColorsByIntColorCode(intColorCode: string) {
+  //   const intColor = await this.prisma.intColor.findUnique({
+  //     where: {
+  //       intColorCode
+  //     }
+  //   })
+
+  //   if (!intColor) {
+  //     throw new Error('intColor not found')
+  //   }
+
+  //   const extColors = await this.prisma.intExtColor.findMany({
+  //     where: { intColorId: intColor.intColorId },
+  //     select: {
+  //       extColor: true
+  //     }
+  //   })
+
+  //   return extColors.map(extColor => extColor.extColor)
+  // }
+
+  // async getOptionsByModelCode(modelCode: string) {
+  //   const carModel = await this.prisma.carModel.findUnique({
+  //     where: {
+  //       modelCode
+  //     }
+  //   })
+
+  //   if (!carModel) {
+  //     throw new Error('carModel not found')
+  //   }
+
+  //   const options = await this.prisma.carModelOption.findMany({
+  //     where: { modelId: carModel.modelId },
+  //     select: {
+  //       option: true
+  //     }
+  //   })
+
+  //   return options.map(option => option.option)
+  // }
+
+  // async getDisabledOptionsBySelectedOptionCodes(optionCodes: string[]) {
+  //   const data: any = []
+  //   await Promise.all(
+  //     optionCodes.map(async optionCode => {
+  //       const option = await this.prisma.option.findUnique({
+  //         where: {
+  //           optionCode
+  //         }
+  //       })
+
+  //       if (!option) {
+  //         throw new Error('option not found')
+  //       }
+
+  //       const disabledOptions = await this.prisma.disabledOption.findMany({
+  //         where: {
+  //           optionId: option.optionId
+  //         },
+  //         select: {
+  //           disabledOption: true
+  //         }
+  //       })
+  //       disabledOptions.map(disabledOption => {
+  //         data.push(disabledOption.disabledOption)
+  //       })
+  //     })
+  //   )
+  //   return data
+  // }
+
+  // async getTuixsByOptionCode(modelCode: string) {
+  //   const carModel = await this.prisma.carModel.findUnique({
+  //     where: {
+  //       modelCode
+  //     }
+  //   })
+
+  //   if (!carModel) {
+  //     throw new Error('carModel not found')
+  //   }
+
+  //   const tuixs = await this.prisma.carModelTuix.findMany({
+  //     where: { modelId: carModel.modelId },
+  //     select: {
+  //       tuix: true
+  //     }
+  //   })
+
+  //   return tuixs.map(tuix => tuix.tuix)
+  // }
+
+  // async getAddPosibleTuixBySelectedOptionCode(optionCodes: string[]) {
+  //   const data: any = []
+  //   await Promise.all(
+  //     optionCodes.map(async optionCode => {
+  //       const option = await this.prisma.option.findUnique({
+  //         where: {
+  //           optionCode
+  //         }
+  //       })
+
+  //       if (!option) {
+  //         throw new Error('option not found')
+  //       }
+
+  //       const tuixRequiredOptions = await this.prisma.tuixRequiredOption.findMany({
+  //         where: {
+  //           optionId: option.optionId
+  //         },
+  //         select: {
+  //           tuix: true
+  //         }
+  //       })
+  //       tuixRequiredOptions.map(tuixRequiredOption => {
+  //         data.push(tuixRequiredOption.tuix)
+  //       })
+  //     })
+  //   )
+  //   return data
+  // }
 }
