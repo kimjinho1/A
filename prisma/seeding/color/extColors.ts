445ae6bac9314b5e3505452f7419afd5165adc3d
kimjinho1 <rlawlsgh8113@naver.com>
2023-07-05 00:49:01 +0900

    feat: 내장색상, 외장색상 이미지 추가

--- prisma/seeding/color/extColors.ts
+++ prisma/seeding/color/extColors.ts
@@ -2,52 +2,64 @@ export const ExtColors = [
   // 아반떼
   {
     extColorCode: 'A5G',
-    extColorName: '아마존 그레이 메탈릭'
+    extColorName: '아마존 그레이 메탈릭',
+    extColorImagePath: '/images/exterior-color/A5G.png'
   },
   {
     extColorCode: 'PE2',
-    extColorName: '에코트로닉 그레이펄'
+    extColorName: '에코트로닉 그레이펄',
+    extColorImagePath: '/images/exterior-color/PE2.png'
   },
   {
     extColorCode: 'M6T',
-    extColorName: '플루이드 그레이 메탈릭'
+    extColorName: '플루이드 그레이 메탈릭',
+    extColorImagePath: '/images/exterior-color/M6T.png'
   },
   {
     extColorCode: 'PM2',
-    extColorName: '메타블루펄'
+    extColorName: '메타블루펄',
+    extColorImagePath: '/images/exterior-color/PM2.png'
   },
   {
     extColorCode: 'YP5',
-    extColorName: '인텐스 블루 펄'
+    extColorName: '인텐스 블루 펄',
+    extColorImagePath: '/images/exterior-color/YP5.png'
   },
   {
     extColorCode: 'SAW',
-    extColorName: '아틀라스 화이트'
+    extColorName: '아틀라스 화이트',
+    extColorImagePath: '/images/exterior-color/SAW.png'
   },
   {
     extColorCode: 'A2B',
-    extColorName: '어비스블랙펄'
+    extColorName: '어비스블랙펄',
+    extColorImagePath: '/images/exterior-color/A2B.png'
   },
   {
     extColorCode: 'C5G',
-    extColorName: '사이버 그레이 메탈릭'
+    extColorName: '사이버 그레이 메탈릭',
+    extColorImagePath: '/images/exterior-color/C5G.png'
   },
   {
     extColorCode: 'R2P',
-    extColorName: '얼티메이트 레드 메탈릭'
+    extColorName: '얼티메이트 레드 메탈릭',
+    extColorImagePath: '/images/exterior-color/R2P.png'
   },
   // 투싼
   {
     extColorCode: 'B6S',
-    extColorName: '실키 브론즈 메탈릭'
+    extColorName: '실키 브론즈 메탈릭',
+    extColorImagePath: '/images/exterior-color/B6S.png'
   },
   {
     extColorCode: 'R2T',
-    extColorName: '쉬머링 실버 메탈릭'
+    extColorName: '쉬머링 실버 메탈릭',
+    extColorImagePath: '/images/exterior-color/R2T.png'
   },
   {
     extColorCode: 'TW3',
-    extColorName: '크리미 화이트 펄'
+    extColorName: '크리미 화이트 펄',
+    extColorImagePath: '/images/exterior-color/TW3.png'
   },
   // 아반떼, 투싼 공통
   // {
@@ -56,18 +68,22 @@ export const ExtColors = [
   // },
   {
     extColorCode: 'R4G',
-    extColorName: '티탄 그레이 메탈릭'
+    extColorName: '티탄 그레이 메탈릭',
+    extColorImagePath: '/images/exterior-color/R4G.png'
   },
   {
     extColorCode: 'PS8',
-    extColorName: '오션 인디고 펄'
+    extColorName: '오션 인디고 펄',
+    extColorImagePath: '/images/exterior-color/PS8.png'
   },
   {
     extColorCode: 'TCM',
-    extColorName: '팬텀 블랙 펄'
+    extColorName: '팬텀 블랙 펄',
+    extColorImagePath: '/images/exterior-color/TCM.png'
   },
   {
     extColorCode: 'Y3G',
-    extColorName: '티타늄 그레이 매트'
+    extColorName: '티타늄 그레이 매트',
+    extColorImagePath: '/images/exterior-color/Y3G.png'
   }
 ]
--- prisma/seeding/color/intColors.ts
+++ prisma/seeding/color/intColors.ts
@@ -2,39 +2,48 @@ export const IntColors = [
   // 아반떼
   {
     intColorCode: 'NNB',
-    intColorName: '블랙모노톤'
+    intColorName: '블랙모노톤',
+    intColorImagePath: '/images/interior-color/NNB.png'
   },
   {
     intColorCode: 'SSS',
-    intColorName: '세이지그린'
+    intColorName: '세이지그린',
+    intColorImagePath: '/images/interior-color/SSS.png'
   },
   {
     intColorCode: 'VHC',
-    intColorName: '캐쉬미어 베이지'
+    intColorName: '캐쉬미어 베이지',
+    intColorImagePath: '/images/interior-color/VHC.png'
   },
   // 투싼
   {
     intColorCode: 'I34',
-    intColorName: '블랙모노톤'
+    intColorName: '블랙모노톤',
+    intColorImagePath: '/images/interior-color/I34.png'
   },
   {
     intColorCode: 'I35',
-    intColorName: '블랙 모노톤(가죽 시트)'
+    intColorName: '블랙 모노톤(가죽 시트)',
+    intColorImagePath: '/images/interior-color/I35.png'
   },
   {
     intColorCode: 'I36',
-    intColorName: '블랙/그레이 투톤(그레이 가죽시트)'
+    intColorName: '블랙/그레이 투톤(그레이 가죽시트)',
+    intColorImagePath: '/images/interior-color/I36.png'
   },
   {
     intColorCode: 'I37',
-    intColorName: '네이비 원톤(인디고 모노톤)'
+    intColorName: '네이비 원톤(인디고 모노톤)',
+    intColorImagePath: '/images/interior-color/I37.png'
   },
   {
     intColorCode: 'I38',
-    intColorName: '블랙모노톤(레드스티치)'
+    intColorName: '블랙모노톤(레드스티치)',
+    intColorImagePath: '/images/interior-color/I38.png'
   },
   {
     intColorCode: 'PRF',
-    intColorName: '블랙 / 브라운 투톤(브라운 가죽시트)'
+    intColorName: '블랙 / 브라운 투톤(브라운 가죽시트)',
+    intColorImagePath: '/images/interior-color/PRF.png'
   }
 ]
