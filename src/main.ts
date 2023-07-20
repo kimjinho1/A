734160ab715722b5035c7138a4ec141d4dfffc8c
kimjinho1 <rlawlsgh8113@naver.com>
2023-07-20 17:52:23 +0900

    feat: 스웨거 설정

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
-import { join } from 'path'
 import { AppModule } from './app.module'
 import { ResponseWrapperInterceptor } from './common/interceptor/response-wrapper.interceptor'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
@@ -12,6 +12,16 @@ async function bootstrap() {
   app.useGlobalInterceptors(new ResponseWrapperInterceptor())
   // 파이프
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }))
+
+  // 스웨거
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build()
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('/docs', app, document)
 
   await app.listen(3000)
 }
