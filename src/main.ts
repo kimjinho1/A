2699d8f79f069fcd17f1988c586f9583bd29c868
kimjinho1 <rlawlsgh8113@naver.com>
2023-07-20 21:11:29 +0900

    remove swagger

--- src/main.ts
+++ src/main.ts
@@ -13,16 +13,6 @@ async function bootstrap() {
   // 파이프
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }))
 
-  // 스웨거
-  const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
-    .setVersion('1.0')
-    .addTag('cats')
-    .build()
-  const document = SwaggerModule.createDocument(app, config)
-  SwaggerModule.setup('/docs', app, document)
-
   await app.listen(3000)
 }
 bootstrap()
